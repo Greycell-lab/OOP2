b851af59457ef0e697d4007515844ec82ab109aa
Greycell <dominik.severin@yandex.com>
2023-01-26 13:10:57 +0100

    Update

--- Tag 7/src/Aufgabe_0/Main.java
+++ Tag 7/src/Aufgabe_0/Main.java
@@ -3,11 +3,11 @@ package Aufgabe_0;
 public class Main {
     public static void main(String[] args) {
         Warenkorb w = new Warenkorb();
-        w.setVersandArt(new StandardVersand());
+        //w.setVersandArt(new StandardVersand());
         w.setVersandArt(new ExpressVersand());
         new Artikel(2.50, "Chips");
         new Artikel(6.30, "Pizza");
-        Artikel.artikelListe.forEach(a -> w.getWarenkorb().put(a, 15));
+        Artikel.artikelListe.forEach(a -> w.getWarenkorb().put(a, 3));
         w.getGesamtWert();
     }
 }
